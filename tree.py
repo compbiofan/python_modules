bfa6ee5976219ede2d84a9b1cc1df32133cf22af
Fan <xf2@nlogin1.nots.rice.edu>
2020-04-04 09:11:15 -0500

    More modules in tree.py.

--- tree.py
+++ tree.py
@@ -1,4 +1,4 @@
-
+import copy
 class node():
     def __init__(self, id, parent=-1, children = [], is_leaf = False):
         self.id = id        
@@ -13,6 +13,16 @@ class node():
         # leaf descendant
         self.leaf_desc = []
 
+# all events on top of a certain node
+def aggregate_events(tree, node):
+    e = copy.deepcopy(tree[node].edge)
+    p = tree[node].parent
+    while p != -1:
+        for i in tree[p].edge:
+            e.append(i)
+        p = tree[p].parent
+    return e
+
 # given a tree, add the levels to each node, starting from root at level 0
 # the tree is from leaves to root
 def add_level_reverse(t):
@@ -22,8 +32,33 @@ def add_level_reverse(t):
             continue
         else:
             # get the parent's level
-            p_level = t[i.parent].level
             i.level = p_level + 1
     return t 
 
+# for every internal node, add the leaf descendants in leaf_desc
+# tree is in reverse order, i.e., leaves' IDs are smaller than internal nodes'
+def add_leaf_desc(tree, tree_order):
+    rg = range(1, len(tree))
+    if tree_order == "forw":
+        rg = rg[:,:,-1] 
 
+    for i in rg:
+        id = i
+        # start from empty before adding
+        tree[i].leaf_desc = []
+        if tree[i].is_leaf:
+            tree[i].leaf_desc.append(id)
+        else:
+            for j in tree[i].children:
+                for x in tree[j].leaf_desc:
+                    tree[i].leaf_desc.append(x)
+
+    return tree
+
+# given a binary tree and a node, report the sibling id 
+def find_sibling(tree, node):
+    cs = tree[tree[node].parent].children
+    for i in cs:
+        if i != node:
+            return i
+    return "NA"
